d4f8a0355828e27bf987f6bef296b48bcd60e0ba
Leonardo Maglio <leotagit@gmail.com>
2017-08-16 15:51:19 +0200

    Panic buy/sell

--- extensions/strategies/leota/strategy.js
+++ extensions/strategies/leota/strategy.js
@@ -10,6 +10,7 @@ module.exports = function container (get, set, clear) {
     getOptions: function () {
       this.option('period', 'period length', String, '2m')
       this.option('diff', 'diff size', String, '5')
+      this.option('panic', 'panic size', String, 'none')
 
     },
 
@@ -25,12 +26,16 @@ module.exports = function container (get, set, clear) {
       } else {
         var my_last_trade = s.my_trades[s.my_trades.length -1]
         // Sell logic
-        if(highest == undefined) {
-          highest = s.period.high
-        } 
         if(my_last_trade.type !== 'sell') {
+          if(highest == undefined) {
+            highest = s.period.high
+          } 
           if(s.period.high > highest) {
             highest = s.period.high
+          }
+          // Panic sell
+          if(typeof(s.options.panic) == 'number' && s.period.high < my_last_trade.price - s.options.panic) {
+            s.signal = 'sell'
           }
           if(s.period.high < (highest - s.options.diff) && s.period.high > my_last_trade.price) {
             lowest = s.period.high
@@ -38,12 +43,16 @@ module.exports = function container (get, set, clear) {
           }
         }
         // Buy logic
-        if(lowest == undefined) {
-          lowest = s.period.low
-        } 
         if(my_last_trade.type !== 'buy') {
+          if(lowest == undefined) {
+            lowest = s.period.low
+          } 
           if(s.period.low < lowest) {
             lowest = s.period.low
+          }
+          // Panic buy
+          if(typeof(s.options.panic) == 'number' && s.period.low > my_last_trade.price + s.options.panic) {
+            s.signal = 'buy'
           }
           if(s.period.low > (lowest + s.options.diff) && s.period.low < my_last_trade.price) {
             highest = s.period.low
